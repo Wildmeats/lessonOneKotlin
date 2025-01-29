9144c9b52347e0bd1977cc26baf11b0a6fc9e4ae
wildmeats <wildmeat1@yandex.ru>
2025-01-29 21:50:43 +0300

    KS-1-2 Creation of employees

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,16 @@
 package org.example.lesson_1
 
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60
+
 fun main() {
     val secondsInSpace = 5340
-    val hours = secondsInSpace / 3600
-    val minutes = (secondsInSpace % 3600) / 60
-    val seconds = secondsInSpace % 60
-    val formattedTime = "%02d:%02d:%02d".format(hours, minutes,seconds)
-    println("$formattedTime Гагарин провел в космосе.")
+
+    val hours = secondsInSpace / SECONDS_IN_HOUR
+    val minutes = (secondsInSpace % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds = secondsInSpace % SECONDS_IN_MINUTE
+
+    val formattedTime = "%02d:%02d:%02d".format(hours, minutes, seconds)
+        println("$formattedTime – Гагарин провел в космосе.")
 
 }

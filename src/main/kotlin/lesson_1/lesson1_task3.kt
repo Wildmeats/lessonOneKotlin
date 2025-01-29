51873f59365c8e11d008accbba62970d3e16b47e
Viktor <wildmeats@mail.ru>
2025-01-29 14:16:29 +0300

    KS-1-3 space flight

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_1
+
+fun main() {
+    val yearOfFlight: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("Год полета - $yearOfFlight \nВзлет - 0$hour:0$minute")
+    hour = 10
+    minute = 55
+    println("Посадка - $hour:$minute")
+}

19dfb959703e55b180a6d71fa76997ae1a356e60
wildmeats <wildmeat1@yandex.ru>
2025-01-31 17:17:48 +0300

    KS-3-5 chess breaking the board

---
+++ src/main/kotlin/lesson_1/lesson3_task5.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val dataLine = "D2-D4;0"
+
+    val colum: Char = 'D'
+    var row: Byte = 2
+    val move: Byte = 0
+
+    println("$colum$row")
+
+    row = 4
+
+    println("$colum$row")
+    println(move)
+}

cfcffaf500fd2b9261b5782ce4e540248b1e8390
wildmeats <wildmeat1@yandex.ru>
2025-02-01 15:18:05 +0300

    KS-3-5 Chess breaking the board

--- src/main/kotlin/lesson_1/lesson3_task5.kt
+++ src/main/kotlin/lesson_1/lesson3_task5.kt
@@ -3,20 +3,15 @@ package org.example.lesson_1
 fun main() {
     val dataLine = "D2-D4;0"
 
-    val parts = dataLine.split(";")
-    val moveInfo = parts[0]
-    val moveFlag = parts[1]
+    val parts = dataLine.split("-", ";")
 
-    val positions = moveInfo.split("-")
-    val startPosition = positions[0]
-    val endPosition = positions[1]
+    println(parts)
 
-    val startColumn = startPosition[0]
-    val startRow = startPosition[1]
-    val endColumn = endPosition[0]
-    val endRow = endPosition[1]
+    val startPosition = parts[0]
+    val endPosition = parts[1]
+    val moveFlag = parts[2]
 
-    println("Откуда: $startColumn$startRow")
-    println("Куда: $endColumn$endRow")
+    println("Откуда: $startPosition")
+    println("Куда: $endPosition")
     println("Номер хода: $moveFlag")
 }

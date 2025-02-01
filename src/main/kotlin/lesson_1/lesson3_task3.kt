d900a638bb023c9d90decc5e207b3735a77bc13d
wildmeats <wildmeat1@yandex.ru>
2025-02-01 10:13:40 +0300

    KS-3-3 multiplication table

--- src/main/kotlin/lesson_1/lesson3_task3.kt
+++ src/main/kotlin/lesson_1/lesson3_task3.kt
@@ -3,13 +3,10 @@ package org.example.lesson_1
 fun main() {
     val number = 3
 
-    println("$number x 1 = ${number * 1}")
-    println("$number x 2 = ${number * 2}")
-    println("$number x 3 = ${number * 3}")
-    println("$number x 4 = ${number * 4}")
-    println("$number x 5 = ${number * 5}")
-    println("$number x 6 = ${number * 6}")
-    println("$number x 7 = ${number * 7}")
-    println("$number x 8 = ${number * 8}")
-    println("$number x 9 = ${number * 9}")
+    println("$number x 1 = ${number * 1} \n$number x 2 = ${number * 2} " +
+    "\n$number x 3 = ${number * 3} \n$number x 4 = ${number * 4}" +
+    "\n$number x 5 = ${number * 5} \n$number x 6 = ${number * 6}" +
+    "\n$number x 7 = ${number * 7} \n$number x 8 = ${number * 8}" +
+    "\n$number x 9 = ${number * 9}")
+
 }

cd9374bde70cde3b6be9ad506ac212052515b45e
wildmeats <wildmeat1@yandex.ru>
2025-02-01 19:07:22 +0300

    KS-5-1 Captcha with example

---
+++ src/main/kotlin/lesson_1/lesson5_task1.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val a = 2
+    val b = 3
+
+    println("Что бы убедиться что вы не бот решите математический пример, ($a + $b = введите результат):")
+
+    val res = readln().toInt()
+
+    if (res == 5) {
+        println("\"Добро пожаловать!\"")
+    } else  {
+        println("\"Доступ запрещен\"")
+    }
+
+
+}

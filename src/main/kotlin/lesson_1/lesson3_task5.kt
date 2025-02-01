b4624ee258fa5d200b667ff10ab0f3596cce47da
wildmeats <wildmeat1@yandex.ru>
2025-02-01 10:29:37 +0300

    KS-3-3 multiplication table

--- src/main/kotlin/lesson_1/lesson3_task5.kt
+++ src/main/kotlin/lesson_1/lesson3_task5.kt
@@ -1,17 +1,22 @@
 package org.example.lesson_1
 
 fun main() {
-
     val dataLine = "D2-D4;0"
 
-    val colum: Char = 'D'
-    var row: Byte = 2
-    val move: Byte = 0
+    val parts = dataLine.split(";")
+    val moveInfo = parts[0]
+    val moveFlag = parts[1]
 
-    println("$colum$row")
+    val positions = moveInfo.split("-")
+    val startPosition = positions[0]
+    val endPosition = positions[1]
 
-    row = 4
+    val startColumn = startPosition[0]
+    val startRow = startPosition[1]
+    val endColumn = endPosition[0]
+    val endRow = endPosition[1]
 
-    println("$colum$row")
-    println(move)
+    println("Откуда: $startColumn$startRow")
+    println("Куда: $endColumn$endRow")
+    println("Номер хода: $moveFlag")
 }

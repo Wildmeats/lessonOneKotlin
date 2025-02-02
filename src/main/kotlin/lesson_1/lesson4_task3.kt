e357dbb9658974c96e84008473c90a7684b02ebf
wildmeats <wildmeat1@yandex.ru>
2025-02-02 11:31:41 +0300

    KS-4-3 Legume growth

--- src/main/kotlin/lesson_1/lesson4_task3.kt
+++ src/main/kotlin/lesson_1/lesson4_task3.kt
@@ -1,14 +1,19 @@
 package org.example.lesson_1
 
+const val BASIC_AIR_HUMIDITY = 20
+const val BASIC_TIME_OF_YEAR = "Зима"
 fun main() {
 
-    var isSunnyWeather: Boolean = true
-    var isOpenAwning: Boolean = true
-    var isAirHumidity: Int = 20
-    var isTimeOfYear: String = "Зима"
+    var currentSunnyWeather: Boolean = true
+    var currentOpenAwning: Boolean = true
+    var currentAirHumidity: Int = 20
+    var currentTimeOfYear: String = "Зима"
 
-    val timeOfYear: Boolean = isTimeOfYear != "Зима"
+    val checkingCondition: Boolean = currentSunnyWeather &&
+            currentOpenAwning &&
+            currentAirHumidity == BASIC_AIR_HUMIDITY &&
+            currentTimeOfYear != BASIC_TIME_OF_YEAR
 
-    println("Благоприятные ли условия сейчас для роста бобовых?" +
-            " ${isSunnyWeather && isOpenAwning && isAirHumidity == 20 && timeOfYear}")
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $checkingCondition")
 }

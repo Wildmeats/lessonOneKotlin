f62d64d53c31de6568c05929ea50581512bda2ee
wildmeats <wildmeat1@yandex.ru>
2025-02-02 16:27:43 +0300

    KS-4-5 Research ship

--- src/main/kotlin/lesson_1/lesson4_task5.kt
+++ src/main/kotlin/lesson_1/lesson4_task5.kt
@@ -8,22 +8,18 @@ fun main() {
 
     println("- наличие повреждений корпуса(true / false):")
     var userInput1: Boolean = readLine()?.toBoolean() ?: false
-    val presenceOfNoDamage = !userInput1
 
     println("- текущий состав экипажа:(Введите число)")
-    var userInput2: Int = readLine()!!.toInt()
-    val crewComposition = userInput2 in MIN_NUM_OF_CREW..MAX_NUM_OF_CREW
+    var userInput2: Int = readLine()?.toInt() ?: 0
 
     println("- количество ящиков с провизией на борту:(Введите число)")
-    var userInput3: Int = readLine()!!.toInt()
-    val provisionsOnBoard = userInput3 > PROVISIONS
+    var userInput3: Int = readLine()?.toInt() ?: 0
 
     println("- благоприятность метеоусловий:(true / false)")
     var userInput4: Boolean = readLine()?.toBoolean() ?: false
-    val weather = userInput4
 
-    val missionCheck = (presenceOfNoDamage && crewComposition && provisionsOnBoard)
-            || (!presenceOfNoDamage && userInput2 == MAX_NUM_OF_CREW && userInput3 >= PROVISIONS && weather)
+    val missionCheck = (!userInput1 && userInput2 in MIN_NUM_OF_CREW..MAX_NUM_OF_CREW && userInput3 > PROVISIONS)
+            || (userInput1 && userInput2 == MAX_NUM_OF_CREW && userInput3 >= PROVISIONS && userInput4)
 
     println(missionCheck)
 }

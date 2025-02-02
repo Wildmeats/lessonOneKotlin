052d655e6f6e967aa8f1249f4b2571189761111b
wildmeats <wildmeat1@yandex.ru>
2025-02-02 16:34:41 +0300

    KS-4-5 Research ship

--- src/main/kotlin/lesson_1/lesson5_task1.kt
+++ src/main/kotlin/lesson_1/lesson5_task1.kt
@@ -2,18 +2,17 @@ package org.example.lesson_1
 
 fun main() {
 
-    val a = 2
-    val b = 3
+    val num1 = 2
+    val num2 = 3
 
-    println("Что бы убедиться что вы не бот решите математический пример, ($a + $b = введите результат):")
+    println("Что бы убедиться что вы не бот решите математический пример, ($num1 + $num2 = введите результат):")
 
-    val res = readln().toInt()
+    val userResult = readln().toInt()
 
-    if (res == 5) {
+    if (userResult == num1 + num2) {
         println("\"Добро пожаловать!\"")
     } else {
         println("\"Доступ запрещен\"")
     }
-
 
 }

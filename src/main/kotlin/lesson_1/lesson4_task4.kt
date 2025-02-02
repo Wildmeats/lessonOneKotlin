43ee25c991b04dc4d2b21235b9f09d7602f19d05
wildmeats <wildmeat1@yandex.ru>
2025-02-02 11:36:51 +0300

    KS-4-4 Training program

--- src/main/kotlin/lesson_1/lesson4_task4.kt
+++ src/main/kotlin/lesson_1/lesson4_task4.kt
@@ -2,11 +2,17 @@ package org.example.lesson_1
 
 fun main() {
 
-        val day = 5
+    val day = 5
 
-        val isHandsAndAbs = day % 2 != 0
-        val isLegsAndBack = day % 2 == 0
+    val isHandsAndAbs = day % 2 != 0
 
-        println("Упражнения для рук:    $isHandsAndAbs \nУпражнения для ног:    ${!isHandsAndAbs}" +
-                "\nУпражнения для спины:  ${!isHandsAndAbs} \nУпражнения для пресса: $isHandsAndAbs")
-    }
+
+    println(
+        """
+                |Упражнения для рук:    $isHandsAndAbs 
+                |Упражнения для ног:    ${!isHandsAndAbs}
+                |Упражнения для спины:  ${!isHandsAndAbs} 
+                |Упражнения для пресса: $isHandsAndAbs
+    """.trimMargin()
+    )
+}
